0d6c7105fc8318b1b0a2010fdd8b8b95f620c1ae
Adam Domurad <adamdomurad@gmail.com>
2021-03-06 12:41:01 -0500

    Add ChoiceMenu component

--- source/inkUtils.tsx
+++ source/inkUtils.tsx
@@ -1,10 +1,120 @@
-import React from 'react';
-import TextInput from 'ink-text-input';
+import React, { useContext, useEffect, useState } from "react";
+import TextInput from "ink-text-input";
+import { useInput, Text, Key, Box } from "ink";
+import { RESOURCES } from "./types";
+import { splitArray } from "./jsUtils";
 
-interface InputProps {
-	onSubmit(text: string): void;
+export interface InputProps {
+  onSubmit(text: string): void;
 }
 export function Input(props: InputProps) {
-	const [query, setQuery] = React.useState('');
-	return <TextInput showCursor={true} value={query} onChange={setQuery} onSubmit={props.onSubmit} />;
+  const [query, setQuery] = React.useState("");
+  return (
+    <TextInput
+      showCursor={true}
+      value={query}
+      onChange={setQuery}
+      onSubmit={props.onSubmit}
+    />
+  );
+}
+
+export interface FocusItemProps {
+  text: string;
+  isFocused: boolean;
+}
+export function FocusableItem(props: FocusItemProps) {
+  if (props.isFocused) {
+    return (
+      <Text>
+        {">"}
+        {props.text}
+      </Text>
+    );
+  } else {
+    return <Text>{props.text}</Text>;
+  }
+}
+
+export interface ChoiceMenuProps {
+  options: string[];
+  onSubmit(text: string): void;
+}
+export function ChoiceMenu(props: ChoiceMenuProps) {
+  const [selected, setSelected] = useState(props.options[0]);
+  const chunks = splitArray(props.options, 4);
+
+  function getOptionXy(option: string) {
+    for (let y = 0; y < chunks.length; y++) {
+      for (let x = 0; x < chunks[y].length; x++) {
+        if (chunks[y][x] === option) {
+          return [x, y];
+        }
+      }
+    }
+    return undefined;
+  }
+
+  useInput((input: string, key: Key) => {
+    // Submit with enter
+    if (key.return) {
+      props.onSubmit(selected);
+      return;
+    }
+    const optionIdx = props.options.indexOf(selected);
+    // Loop until we hit the same value again, then exit
+    for (
+      let i = (optionIdx + 1) % props.options.length;
+      i != optionIdx;
+      i = (i + 1) % props.options.length
+    ) {
+      if (props.options[i].toLowerCase().startsWith(input.toLowerCase())) {
+        setSelected(props.options[i]);
+        return;
+      }
+    }
+    let [x, y] = getOptionXy(selected) || [0, 0];
+    if (key.leftArrow) {
+      x -= 1;
+    }
+    if (key.tab || key.rightArrow) {
+      x += 1;
+    }
+    if (key.downArrow) {
+      y += 1;
+    }
+    if (key.upArrow) {
+      y -= 1;
+    }
+    y = (y + chunks.length) % chunks.length;
+    x = (x + chunks[0].length) % chunks[0].length;
+    const chunkValueOrUndefined = (chunks[y] || [])[x];
+    setSelected(chunkValueOrUndefined || props.options[0]);
+  });
+
+  return (
+    <Box padding={2} width={80} flexDirection="column">
+      {chunks.map(toRow)}
+    </Box>
+  );
+  function toRow(chunk: string[]) {
+    return (
+      <Box key={chunk[0]} width={80} flexDirection="row">
+        {chunk.map((option) => (
+          <Box key={option} width={20}>
+            <FocusableItem text={option} isFocused={option === selected} />
+          </Box>
+        ))}
+      </Box>
+    );
+  }
+}
+
+export function ChoiceTest() {
+  return (
+    <ChoiceMenu
+      onSubmit={(option) => console.log(option)}
+      options={RESOURCES}
+    />
+  );
 }
--- source/jsUtils.tsx
+++ source/jsUtils.tsx
@@ -1,13 +1,13 @@
 export function sum(elems, f) {
-    let val = 0;
-    for (const elem of elems) {
-        val += f(elem); 
-    }
-    return val;
+  let val = 0;
+  for (const elem of elems) {
+    val += f(elem);
+  }
+  return val;
 }
 
 export function clamp(value: number, min: number, max: number) {
-    return Math.max(min, Math.min(value, max))
+  return Math.max(min, Math.min(value, max));
 }
 
 export const rand = (arr) => arr[Math.floor(Math.random() * arr.length)];
@@ -17,7 +17,7 @@ export const rand = (arr) => arr[Math.floor(Math.random() * arr.length)];
  * Returns a random number between min (inclusive) and max (exclusive)
  */
 export function randRange(min, max) {
-    return Math.random() * (max - min) + min;
+  return Math.random() * (max - min) + min;
 }
 
 /**
@@ -29,114 +29,133 @@ export function randRange(min, max) {
  * Using Math.round() will give you a non-uniform distribution!
  */
 export function randIntRange(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 export function range(n) {
-    let ret:number[] = [];
-    for (var i = 0; i < n; i++) {
-        ret.push(i);
-    }
-    return ret;
+  let ret: number[] = [];
+  for (var i = 0; i < n; i++) {
+    ret.push(i);
+  }
+  return ret;
 }
 
 export function countAll<T>(objs: T[], obj: T) {
-    let sum = 0;
-    for (const o of objs) {
-        if (o === obj) {
-            sum++;
-        }
+  let sum = 0;
+  for (const o of objs) {
+    if (o === obj) {
+      sum++;
     }
-    return sum;
+  }
+  return sum;
 }
 
 export function hash(str: string) {
-    let hash = 1;
-    if (str.length === 0) {
-        return hash;
-    }
-    for (let i = 0; i < str.length; i++) {
-        const chr = str.charCodeAt(i);
-        hash  = ((hash << 5) - hash) + chr;
-        hash |= 0; // Convert to 32bit integer
-    }
-    // Dont allow a hash of 0
-    return hash || 1;
+  let hash = 1;
+  if (str.length === 0) {
+    return hash;
+  }
+  for (let i = 0; i < str.length; i++) {
+    const chr = str.charCodeAt(i);
+    hash = (hash << 5) - hash + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  // Dont allow a hash of 0
+  return hash || 1;
 }
 
 export function random<T>(arr: T[]) {
-    return arr[Math.floor(Math.random() * arr.length)];
+  return arr[Math.floor(Math.random() * arr.length)];
 }
 
 export function randomN<T>(arr: T[], n: number): T[] {
-    const ret:T[] = [];
-    for (let i = 0; i < Math.min(n, arr.length); i++) {
-        let elem = random(arr);
-        while (ret.indexOf(elem) >= 0) {
-            elem = random(arr);
-        }
-        ret.push(elem);
+  const ret: T[] = [];
+  for (let i = 0; i < Math.min(n, arr.length); i++) {
+    let elem = random(arr);
+    while (ret.indexOf(elem) >= 0) {
+      elem = random(arr);
     }
-    return ret;
+    ret.push(elem);
+  }
+  return ret;
 }
 
-export function has<T>(arr:T[], elem:T) {
-    return arr.indexOf(elem) >= 0;
+export function has<T>(arr: T[], elem: T) {
+  return arr.indexOf(elem) >= 0;
 }
 
-export function any<T>(arr:T[], func:(T)=>boolean) {
-    for (const elem of arr) {
-        if (func(elem)) {
-            return true;
-        }
+export function any<T>(arr: T[], func: (T) => boolean) {
+  for (const elem of arr) {
+    if (func(elem)) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 export function hasProperty(obj: any, key: string) {
-    return Object.prototype.hasOwnProperty.call(obj, key);
+  return Object.prototype.hasOwnProperty.call(obj, key);
 }
 
 export function getProperty(obj: any, key: string) {
-    return hasProperty(obj, key) ? obj[key] : undefined;
+  return hasProperty(obj, key) ? obj[key] : undefined;
 }
 
-export function assign<A, B extends A>(obj1:B, obj2: A): B {
-    return Object.assign({}, obj1, obj2);
+export function assign<A, B extends A>(obj1: B, obj2: A): B {
+  return Object.assign({}, obj1, obj2);
 }
 
 export function append<T>(list: T[], obj: T) {
-    return [...list, obj];
+  return [...list, obj];
 }
 
 export function removeOne<T>(list: T[], obj: T): boolean {
-    const idx = list.indexOf(obj);
-    if (idx >= 0) {
-        list.splice(idx, 1);
-        return true;
-    }
-    return false;
+  const idx = list.indexOf(obj);
+  if (idx >= 0) {
+    list.splice(idx, 1);
+    return true;
+  }
+  return false;
 }
 
 export function interpolate(a, b, precision) {
-    return a + (b - a) * precision;
+  return a + (b - a) * precision;
 }
 
 export function anyTrue(list, f) {
-    for (let elem of list) {
-        if (f(elem)) {
-            return true;
-        }
+  for (let elem of list) {
+    if (f(elem)) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 export function nOf(n, elem) {
-    const ret:any[] = [];
-    for (let i = 0; i < n; i++) {
-        ret.push(elem);
-    }
-    return ret;
+  const ret: any[] = [];
+  for (let i = 0; i < n; i++) {
+    ret.push(elem);
+  }
+  return ret;
+}
+
+/**
+ * Returns an array with arrays of the given size.
+ *
+ * @param myArray {Array} array to split
+ * @param size {Integer} Size of every group
+ */
+export function splitArray(myArray, size) {
+  const arrayLength = myArray.length;
+  const chunks: any[] = [];
+
+  for (let index = 0; index < arrayLength; index += size) {
+    const myChunk = myArray.slice(index, index + size);
+    // Do something if you want with the group
+    chunks.push(myChunk);
+  }
+
+  return chunks;
 }
